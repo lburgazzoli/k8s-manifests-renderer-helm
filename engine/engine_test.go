5d8c907241fcda32bcdf3df478b611b398ca7aee
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-08-13 11:24:07 +0200

    add some tests

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -1,6 +1,7 @@
 package engine_test
 
 import (
+	"github.com/lburgazzoli/helm-libs/engine/customizers"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -18,30 +19,61 @@ func TestEngine(t *testing.T) {
 	e := engine.New()
 	g.Expect(e).ShouldNot(BeNil())
 
-	c, err := e.Load(engine.ChartSpec{
+	cs := engine.ChartSpec{
 		Repo:    "https://dapr.github.io/helm-charts/",
 		Name:    "dapr",
 		Version: "1.13.5",
+	}
+
+	t.Run("with_defaults", func(t *testing.T) {
+		c, err := e.Load(cs)
+		g.Expect(err).ShouldNot(HaveOccurred())
+		g.Expect(c).ShouldNot(BeNil())
+
+		r, err := c.Render(
+			t.Name(),
+			xid.New().String(),
+			0,
+			map[string]interface{}{
+				"dapr_operator": map[string]interface{}{
+					"replicaCount": 5,
+				},
+			},
+			nil)
+
+		g.Expect(err).ShouldNot(HaveOccurred())
+		g.Expect(r).ShouldNot(BeEmpty())
+
+		g.Expect(r).To(
+			ContainElement(
+				jq.Match(`.metadata.name == "dapr-operator" and .spec.replicas == 5`)))
 	})
 
-	g.Expect(err).ShouldNot(HaveOccurred())
-	g.Expect(c).ShouldNot(BeNil())
+	t.Run("with_customizer", func(t *testing.T) {
+		c, err := e.Load(
+			cs,
+			engine.WithCustomizer(customizers.JQ(`.dapr_operator.replicaCount = 6`)),
+		)
 
-	r, err := c.Render(
-		t.Name(),
-		xid.New().String(),
-		0,
-		map[string]interface{}{
-			"dapr_operator": map[string]interface{}{
-				"replicaCount": 5,
+		g.Expect(err).ShouldNot(HaveOccurred())
+		g.Expect(c).ShouldNot(BeNil())
+
+		r, err := c.Render(
+			t.Name(),
+			xid.New().String(),
+			0,
+			map[string]interface{}{
+				"dapr_operator": map[string]interface{}{
+					"replicaCount": 5,
+				},
 			},
-		},
-		nil)
+			nil)
 
-	g.Expect(err).ShouldNot(HaveOccurred())
-	g.Expect(r).ShouldNot(BeEmpty())
+		g.Expect(err).ShouldNot(HaveOccurred())
+		g.Expect(r).ShouldNot(BeEmpty())
 
-	g.Expect(r).To(
-		ContainElement(
-			jq.Match(`.metadata.name == "dapr-operator" and .spec.replicas == 5`)))
+		g.Expect(r).To(
+			ContainElement(
+				jq.Match(`.metadata.name == "dapr-operator" and .spec.replicas == 6`)))
+	})
 }
